6fabc5111f5db0f381ecef5981593d15098d5c51
Evgeniya-just <113514729+Evgeniya-just@users.noreply.github.com>
2022-11-05 21:06:11 +0300

    feat(lw0 Exercise1): create Exercise1

---
+++ lw0/Exercise1.dart
@@ -0,0 +1,23 @@
+import "dart:io";
+
+void fizzbuzz(int a){
+  if (a%15==0){
+    print("fizzbuzz");
+  }
+  else if(a%3==0){
+    print("fizz");
+  }
+  else if (a%5==0){
+    print("buzz");
+  }
+  else{
+    print(a);
+  }
+}
+void main() {
+  int? n = int.parse(stdin.readLineSync()!);
+  print("---------------------");
+  for (var i = 1; i <= n; i++) {
+    fizzbuzz(i);
+  }
+}
